57d74be7d8eac4acc3d8dfe5c939a6d69c068964
dgw <dgw@technobabbl.es>
2023-05-13 16:44:20 -0500

    Tests for reddit-hosted image URL handling

--- tests/test_url_matching.py
+++ tests/test_url_matching.py
@@ -94,3 +94,20 @@ def test_short_url_matching(proto, base, trailing_slash, bot):
 
     assert len(matched_rules) == 1
     assert matched_rules[0].get_rule_label() == 'post_or_comment_info'
+
+
+@pytest.mark.parametrize('proto', ('http://', 'https://'))
+@pytest.mark.parametrize('subdomain', ('i', 'preview'))
+@pytest.mark.parametrize('ext', ('jpg', 'jpeg', 'png', 'gif', 'mp4'))
+def test_hosted_image_url_matching(proto, subdomain, ext, bot):
+    link = proto + subdomain + '.redd.it/yib0zwk1mmza1.' + ext
+
+    line = PreTrigger(bot.nick, ':User!user@irc.libera.chat PRIVMSG #channel {}'.format(link))
+    matched_rules = [
+        # we can ignore matches that don't come from this plugin
+        match[0] for match in bot.rules.get_triggered_rules(bot, line)
+        if match[0].get_plugin_name() == 'reddit'
+    ]
+
+    assert len(matched_rules) == 1
+    assert matched_rules[0].get_rule_label() == 'image_info'
